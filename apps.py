1d51abebe35d77b74296ca9a2c241c23332070a5
AvikChakraborty02 <avikchakra016@gmail.com>
2025-03-02 16:26:11 +0530

    Adding Project Files to Github

---
+++ apps.py
@@ -0,0 +1,106 @@
+import requests
+import os
+import io
+from xhtml2pdf import pisa
+from dotenv import load_dotenv
+
+def get_response(city_name,metric):
+    # Load environment variables from the .env file
+    load_dotenv()
+
+    # Access the secret key
+    api_key = os.getenv('WEATHER_API_KEY')
+
+    url = "https://api.weatherstack.com/current?access_key="+api_key+"&query="
+    # splitting the city name and country
+    temp_list=city_name.split(",") 
+
+    # getting the city name only
+    city=temp_list[0] 
+    
+    # concating the city name with the url for query parameter
+    url+=city
+
+    # if metric is given by default it is metric
+    if metric:
+        if metric=='Metric':
+            url+="&units=m"
+        elif metric=='Scientific':
+            url+="&units=s"
+        elif metric=='Fahrenheit':
+            url+="&units=f"
+
+    response=requests.get(url)
+    return (response.json())
+
+def convert_html_to_pdf(html_string):
+    # Create a BytesIO buffer to write the PDF
+    pdf_buffer = io.BytesIO()
+    
+    # Create PDF from HTML string and write it to the buffer
+    pisa_status = pisa.CreatePDF(html_string, dest=pdf_buffer)
+    
+    # Move the cursor back to the start of the BytesIO buffer
+    pdf_buffer.seek(0)
+    
+    # Check if there was an error while creating the PDF
+    if pisa_status.err:
+        return None  # Return None if there was an error generating the PDF
+    
+    # Return the PDF content as bytes
+    return pdf_buffer.read()  # Return the actual PDF content as bytes
+    
+
+def generate_dynamic_html(name,country,region,lat,lon,localtime,utc_offset,image_url,temperature,description,feelslike,wind_speed,wind_degree,wind_direction,humidity,pressure,precip,cloudcover,visibility,uvindex):
+    html = f'''
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Weather Report</title>
+    </head>
+    <body>
+        <h1 style="text-align: center;font-family: 'Times New Roman', Times, serif;">Skycast</h1>
+        <hr>
+        <h3>Location Details:</h3>
+        <hr>
+        <p style="line-height:0.6;font-family: 'Times New Roman', Times, serif;">Name: {name}</p>
+        <p style="line-height:0.6;font-family: 'Times New Roman', Times, serif;">Country: {country}</p>
+        <p style="line-height:0.6;font-family: 'Times New Roman', Times, serif;">Region: {region}</p>
+        <p style="line-height:0.6;font-family: 'Times New Roman', Times, serif;">Latitude: {lat}</p>
+        <p style="line-height:0.6;font-family: 'Times New Roman', Times, serif;">Longitude: {lon}</p>
+        <p style="line-height:0.6;font-family: 'Times New Roman', Times, serif;">Local Time: {localtime}</p>
+        <p style="line-height:0.6;font-family: 'Times New Roman', Times, serif;">UTC Offset: {utc_offset}</p>
+        <hr>
+        <h3>Temperature Details:</h3> 
+        <hr>
+        <div style="text-align: center;">
+            <img src="{image_url}" alt="Weather image" style="border-radius: 5px; margin: auto;">
+        </div>
+        <br>
+        <p style="line-height:0.6;font-family: 'Times New Roman', Times, serif;">Temperature: {temperature}</p>
+        <p style="line-height:0.6;font-family: 'Times New Roman', Times, serif;">Weather Condition: {description}</p>
+        <p style="line-height:0.6;font-family: 'Times New Roman', Times, serif;">Feels Like: {feelslike}</p>
+        <hr>
+        <h3>Wind Details:</h3>
+        <hr>
+        <p style="line-height:0.6;font-family: 'Times New Roman', Times, serif;">Wind Speed: {wind_speed}</p>
+        <p style="line-height:0.6;font-family: 'Times New Roman', Times, serif;">Wind Degree: {wind_degree}</p>
+        <p style="line-height:0.6;font-family: 'Times New Roman', Times, serif;">Wind Direction: {wind_direction}</p>
+        <p style="line-height:0.6;font-family: 'Times New Roman', Times, serif;">Humidity: {humidity}%</p>
+        <p style="line-height:0.6;font-family: 'Times New Roman', Times, serif;">Pressure: {pressure} mb</p>
+        <p style="line-height:0.6;font-family: 'Times New Roman', Times, serif;">Precipitation: {precip}</p>
+        <hr>
+        <h3>More Details:</h3>
+        <hr>
+        <p style="line-height:0.6;font-family: 'Times New Roman', Times, serif;">Cloud Cover: {cloudcover}%</p>
+        <p style="line-height:0.6;font-family: 'Times New Roman', Times, serif;">Visibility: {visibility}</p>
+        <p style="line-height:0.6;font-family: 'Times New Roman', Times, serif;">UV Index: {uvindex}</p> 
+        <h1 style="text-align:end;font-family: 'Times New Roman', Times, serif;">Developed By Avik Chakraborty</h1>
+    </body>
+    </html>
+    '''
+    return html
+
+
---
+++ ui.py
@@ -0,0 +1,290 @@
+import streamlit as st
+import geonamescache
+import apps 
+import asyncio
+import pdfkit
+from datetime import datetime
+
+# Page Configuration
+st.set_page_config(
+    page_title="Skycast⛅",  # Tab title
+    page_icon="⛅",            # Favicon (can use a path to an image file or an emoji)
+    layout="wide",             # Layout of the app (either "centered" or "wide")
+    initial_sidebar_state="expanded"  # Sidebar state ("expanded" or "collapsed")
+)
+
+# Sidebar
+st.sidebar.header("About Us")
+st.sidebar.write("Skycast is a web application where you can query about weather reports of a particular location and you will get the current weather report of that location.")
+st.sidebar.write("Want to know about weather? Go Skycast and ask...")
+st.sidebar.markdown("""<hr>""",unsafe_allow_html=True)
+st.sidebar.header("Contact Us")
+
+# Your social media URLs
+facebook_url = "https://www.facebook.com/share/15b3wweLft/"
+instagram_url = "https://www.instagram.com/ig_mr.starkop/profilecard/?igsh=ZDFjNGtpdW9iaGpn"
+linkedin_url = "https://www.linkedin.com/in/avik-chakraborty-61493824b"
+
+# Include the Font Awesome CDN link to display icons
+st.sidebar.markdown("""
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+""", unsafe_allow_html=True)
+
+# HTML + CSS to create the icons with hover effect
+st.sidebar.markdown(f"""
+    <style>
+        .social-icon {{
+            font-size: 20px;
+            margin: 0 10px;
+            text-decoration: none;
+            color: white;
+            transition: transform 0.3s ease, color 0.3s ease;
+        }}
+
+        .social-icon:hover {{
+            transform: scale(1.2);
+        }}
+
+        .social-icon-facebook:hover {{
+            color: #3b5998; /* Facebook blue */
+        }}
+
+        .social-icon-instagram:hover {{
+            color: #e1306c; /* Instagram pink */
+        }}
+
+        .social-icon-linkedin:hover {{
+            color: #0077b5; /* LinkedIn blue */
+        }}
+    </style>
+
+    <div style="text-align: center;">
+        <a href="{facebook_url}" class="social-icon social-icon-facebook" target="_blank">
+            <i class="fab fa-facebook"></i>
+        </a>
+        <a href="{instagram_url}" class="social-icon social-icon-instagram" target="_blank">
+            <i class="fab fa-instagram"></i>
+        </a>
+        <a href="{linkedin_url}" class="social-icon social-icon-linkedin" target="_blank">
+            <i class="fab fa-linkedin"></i>
+        </a>
+    </div>
+""", unsafe_allow_html=True)
+st.sidebar.markdown("""
+            <br>
+            <p style="line-height:0.6;">Developed By,</p>
+            <p style="line-height:0.6;">Avik Chakraborty 😎</p>
+            """,unsafe_allow_html=True)   
+
+# Title
+st.title("Skycast⛅")
+
+# Geo Cities Names
+gc = geonamescache.GeonamesCache()
+cities = gc.get_cities()
+city_names=['']
+for city in cities.values():
+    city_name=city['name']
+    city_country=city['countrycode']
+    city_names.append(city_name+","+city_country)
+
+# Search Box    
+select_city=st.selectbox("Search a City",city_names)
+select_unit=st.selectbox("Select a Unit",['Metric','Scientific','Fahrenheit'])
+
+# Check if the selectbox has a value selected
+if select_city == "" or select_unit=="":
+    # Disable the button if no option is selected
+    button_disabled = True
+    st.warning("Please select an option before clicking the button.")
+else:
+    # Enable the button if an option is selected
+    button_disabled = False
+
+# Unit Descriptions
+# Create 3 columns
+col1, col2, col3 = st.columns(3)
+
+# Define the content of each card
+with col1:
+    st.markdown("""
+        <div style="border: 1px solid white; padding: 20px; border-radius: 10px; background-color: #1d252d;">
+            <h3>Metric</h3>
+            <p style="line-height:0.7">Temperature: Celsius</p>
+        	<p style="line-height:0.7">Wind Speed/Visibility: Kilometers/Hour</p>
+	        <p style="line-height:0.7">Pressure: MB - Millibar</p>
+	        <p style="line-height:0.7">Precip: MM - Millimeters</p>
+        	<p style="line-height:0.7">Total Snow: CM - Centimeters</p>
+        </div>
+    """, unsafe_allow_html=True)
+
+with col2:
+    st.markdown("""
+        <div style="border: 1px solid white; padding: 20px; border-radius: 10px; background-color: #1d252d;">
+            <h3>Scientific</h3>
+            <p style="line-height:0.7">Temperature: Kelvin</p>
+        	<p style="line-height:0.7">Wind Speed/Visibility: Kilometers/Hour</p>
+	        <p style="line-height:0.7">Pressure: MB - Millibar</p>
+	        <p style="line-height:0.7">Precip: MM - Millimeters</p>
+        	<p style="line-height:0.7">Total Snow: CM - Centimeters</p>
+        </div>
+    """, unsafe_allow_html=True)
+
+with col3:
+    st.markdown("""
+        <div style="border: 1px solid white; padding: 20px; border-radius: 10px; background-color: #1d252d;">
+            <h3>Fahrenheit</h3>
+            <p style="line-height:0.7">Temperature: Fahrenheit</p>
+        	<p style="line-height:0.7">Wind Speed/Visibility: Miles/Hour</p>
+	        <p style="line-height:0.7">Pressure: MB - Millibar</p>
+	        <p style="line-height:0.7">Precip: IN - Inches</p>
+        	<p style="line-height:0.7">Total Snow: IN - Inches</p>
+        </div>
+    """, unsafe_allow_html=True)
+st.markdown("""<br>""",unsafe_allow_html=True)
+# Query Button
+if st.button("Get Weather Report",disabled=button_disabled):
+    with st.spinner("Fetching weather reports..."):
+        response=apps.get_response(select_city,select_unit)
+        if response.get("success")==None:
+
+            st.header(f"Weather Report of {select_city}")
+            st.markdown("""<hr>""",unsafe_allow_html=True)
+            col1, col2, col3, col4 = st.columns(4)
+
+            # Column 1
+            with col1:
+                # Getting Values
+                image=response.get("current").get("weather_icons")
+                image_url=image[0]
+                temperature=str(response.get("current").get("temperature"))
+                desc=response.get("current").get("weather_descriptions")
+                description=desc[0]
+                feelslike=str(response.get("current").get("feelslike"))
+
+                # Formatting temparature
+                if select_unit=="Metric":
+                    temperature+="°C"
+                elif select_unit=="Scientific":
+                    temperature+="K"
+                elif select_unit=="Fahrenheit":
+                    temperature+="°F"
+
+                # Formatting feelslike
+                if select_unit=="Metric":
+                    feelslike+="°C"
+                elif select_unit=="Scientific":
+                    feelslike+="K"
+                elif select_unit=="Fahrenheit":
+                    feelslike+="°F"
+
+                st.markdown(f"""
+                    <div style="border: 1px solid white; padding: 20px; border-radius: 10px; background-color: #1d252d;">
+                        <h5>Weather Details ☁️:</h5>
+                        <div style="text-align: center;">
+                            <img src="{image_url}" alt="Weather image" style="border-radius: 5px; margin: auto;">
+                        </div>
+                        <br>
+                        <p style="line-height:0.6">Temperature: {temperature}</p>
+                        <p style="line-height:0.6">Weather Condition: {description}</p>
+                        <p style="line-height:0.6">Feels Like: {feelslike}</p>         
+                    </div>""",unsafe_allow_html=True)
+            
+            # Column 2
+            with col2:
+                # Getting Values
+                name=response.get("location").get("name")
+                country=response.get("location").get("country")
+                region=response.get("location").get("region")
+                lat=response.get("location").get("lat")
+                lon=response.get("location").get("lon")
+                localtime=response.get("location").get("localtime")
+                utc_offset=response.get("location").get("utc_offset")
+
+                st.markdown(f"""
+                    <div style="border: 1px solid white; padding: 20px; border-radius: 10px; background-color: #1d252d;">
+                        <h5>Location Details 📍:</h5>
+                        <p style="line-height:0.6">Name: {name}</p>
+                        <p style="line-height:0.6">Country: {country}</p>
+                        <p style="line-height:0.6">Region: {region}</p>
+                        <p style="line-height:0.6">Latitude: {lat}</p>
+                        <p style="line-height:0.6">Longitude: {lon}</p>
+                        <p style="line-height:0.6">Local Time: {localtime}</p>
+                        <p style="line-height:0.6">UTC Offset: {utc_offset}</p>          
+                    </div>""",unsafe_allow_html=True)
+            
+            # column 3:
+            with col3:
+                # Getting Values
+                wind_speed=str(response.get("current").get("wind_speed"))
+                wind_degree=response.get("current").get("wind_degree")
+                wind_direction=response.get("current").get("wind_dir")
+                humidity=response.get("current").get("humidity")
+                pressure=response.get("current").get("pressure")
+                precip=str(response.get("current").get("precip"))
+
+                # Formatting Wind Speed
+                if select_unit=="Metric" or select_unit=="Scientific":
+                    wind_speed+=" km/hr"
+                elif select_unit=="Fahrenheit":
+                    wind_speed+=" miles/hr"
+
+                # Formatting Precipitation
+                if select_unit=="Metric" or select_unit=="Scientific":
+                    precip+=" mm"
+                elif select_unit=="Fahrenheit":
+                    precip+=" in"
+
+                st.markdown(f"""
+                    <div style="border: 1px solid white; padding: 20px; border-radius: 10px; background-color: #1d252d;">
+                        <h5>Wind Details 🍃:</h5>
+                        <p style="line-height:0.6">Wind Speed: {wind_speed}</p>
+                        <p style="line-height:0.6">Wind Degree: {wind_degree}</p>
+                        <p style="line-height:0.6">Wind Direction: {wind_direction}</p>
+                        <p style="line-height:0.6">Humidity: {humidity}%</p>
+                        <p style="line-height:0.6">Pressure: {pressure} mb</p>
+                        <p style="line-height:0.6">Precipitation: {precip}</p>         
+                    </div>""",unsafe_allow_html=True)
+                
+            # column 4
+            with col4:
+                # Getting Values
+                cloudcover=response.get("current").get("cloudcover")
+                visibility=str(response.get("current").get("visibility"))
+                uvindex=response.get("current").get("uv_index")
+
+                # Formatting Visibility
+                if select_unit=="Metric" or select_unit=="Scientific":
+                    visibility+=" km"
+                elif select_unit=="Fahrenheit":
+                    visibility+=" miles"
+
+                st.markdown(f"""
+                    <div style="border: 1px solid white; padding: 20px; border-radius: 10px; background-color: #1d252d;">
+                        <h5>More Details:</h5>
+                        <p style="line-height:0.6">Cloud Cover: {cloudcover}%</p>
+                        <p style="line-height:0.6">Visibility: {visibility}</p>
+                        <p style="line-height:0.6">UV Index: {uvindex}</p>        
+                    </div>""",unsafe_allow_html=True)
+            
+            col1,col2,col3=st.columns(3)
+            with col1:
+                pass
+            with col2:
+                st.markdown("""<br>""",unsafe_allow_html=True)
+                html=apps.generate_dynamic_html(name,country,region,lat,lon,localtime,utc_offset,image_url,temperature,description,feelslike,wind_speed,wind_degree,wind_direction,humidity,pressure,precip,cloudcover,visibility,uvindex)
+                pdf_data=apps.convert_html_to_pdf(html)
+                if pdf_data:
+                    st.download_button(
+                        label="Download as PDF",
+                        data=pdf_data,
+                        file_name='Weather Report.pdf',
+                        mime='application/pdf'
+                    )      
+                else:
+                    st.error("Failed to generate PDF")
+            with col3:
+                pass
+
+        elif response.get("success")==False:
+            st.error(str(response["error"]["code"])+" : "+response["error"]["type"]+" - "+response["error"]["info"])           
